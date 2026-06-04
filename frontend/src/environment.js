794dc9933bf88af53144de7b1e39427e3ca97f8f
satyam shukla <satyamshukla9528@gmail.com>
2026-06-04 11:50:44 +0530

    deploying frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,4 +1,15 @@
-let IS_PROD = false;
-const server = "http://localhost:8000";
+// let IS_PROD = false;
+// const server = "http://localhost:8000";
+
+// export default server;
+
+  let IS_PROD = true;
+
+  const server = IS_PROD ?  
+     "https://zoom-video-call-wv6p.onrender.com" :
+
+     "http://localhost:8000"
+     
+
 
 export default server;
